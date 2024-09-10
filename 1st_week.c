fa6b16fba2a6af48943a2fa8f02d95849230abf2
hyunwoo0222 <gusdn4741@naver.com>
2024-09-10 15:20:05 +0900

    2주차 과제

---
+++ 1st_week.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main (void)
+{
+    //Hello World 입력 후 줄 바꿈
+    /*
+    제작 날짜-2024.09.02 마지막 업데이트-2024.09.03 */
+    //printf("Hello world!\n");
+    //printf("%d",124);
+    /*
+    printf("My name:%d\n",20);
+    printf("%d is my point \n",100);
+    printf("Good \nMoring \neverybody\n");
+    */
+    
+    //printf("조현우\n조 현 우\n조  현  우");
+    
+    /*
+    printf("My name: 조현우\n");
+    printf("Address: 사당로 90 \n");
+    printf("My phone num: 010-8731-8076\n");
+    */
+    
+
+    printf("이름:조현우\n");
+    printf("학번:%d \n",202210978);
+
+    return 0;
+}
