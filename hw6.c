795ea77308524f089f45324643e3cb876834421c
hyunwoo0222 <gusdn4741@naver.com>
2024-10-29 14:09:49 +0900

    Add files via upload

---
+++ hw6.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int numary[5];
+    int oddary[5];
+    int evenary[5];
+
+    int a=0,b=0;
+
+    printf("plase input five integer:");
+
+    for (int i = 0; i < sizeof(numary)/sizeof(int); i++)
+    {
+
+        scanf("%d", &numary[i]);
+        if (numary[i]%2!=0)
+        {
+            oddary[a]=numary[i];
+            a++;
+        }
+        else
+        {
+            evenary[b]=numary[i];
+            b++;
+        }
+    }
+
+    printf("odd number:");
+    for (int i = 0; i < a; i++)
+    {
+        printf(" %d",oddary[i]);
+    }
+    printf("\neven number:");
+    for (int i = 0; i < b; i++)
+    {
+        printf(" %d",evenary[i]);
+    }
+    
+    
+     
+}
